94b8113eedd474cd0ae7691b76d69817a2bf6c75
Phagos-21 <ruchinskasmark@gmail.com>
2025-05-20 23:25:26 -0700

    Fix port binding for Render

--- server.py
+++ server.py
@@ -229,4 +229,5 @@ def get_current_user():
 
 # Запуск сервера
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render выдаёт порт через переменную окружения
+    app.run(host='0.0.0.0', port=port, debug=True)
